c7e574f1d8249441beb07ebafc29f9098b9b03bc
DevMessias <messiasbento944@gmail.com>
2024-04-03 15:00:58 -0300

    Feat: Página de cadastro

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,9 +1,50 @@
+import { useState } from 'react';
+
+import logo from '../../assets/logo.png';
+
+import { Link } from 'react-router-dom';
 
 function SignUp(){
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
     return(
-        <>
-            <h1>Pagina Cadastro</h1>
-        </>
+        <div className='container-center' >
+            <div className='login' >
+                <div className='login-area' >
+                    <img src={logo} alt="Logo do sistema" />
+                </div>
+                <form>
+
+                    <h1>Nova Conta</h1>
+                    <input 
+                        type="text" 
+                        placeholder='email@email.com' 
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                    />
+
+                    <input 
+                        type="text" 
+                        placeholder='email@email.com' 
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+
+                    <input 
+                        type="text" 
+                        placeholder='******' 
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+
+                    <button>Cadastrar</button>
+                </form>
+
+                <Link to="/" > Já possui uma conta? Faça login </Link>
+            </div>
+        </div>
     )
 }
 
