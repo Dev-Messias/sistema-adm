0db8c04dd86a2db516850131c7bfb19e3b34129c
DevMessias <messiasbento944@gmail.com>
2024-04-03 13:58:46 -0300

    Inicial

---
+++ src/pages/SignUp/index.jsx
@@ -0,0 +1,10 @@
+
+function SignUp(){
+    return(
+        <>
+            <h1>Pagina Cadastro</h1>
+        </>
+    )
+}
+
+export default SignUp;
---
+++ src/pages/Signin/index.jsx
@@ -0,0 +1,10 @@
+
+function Signin(){
+    return(
+        <>
+            <h1>Pagina Login</h1>
+        </>
+    )
+}
+
+export default Signin;
