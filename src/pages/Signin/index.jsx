0601156eceaaf6b61f459d61f782d0b3fae2ef0e
DevMessias <messiasbento944@gmail.com>
2024-04-03 14:40:55 -0300

    Feat: Página de login

--- src/pages/Signin/index.jsx
+++ src/pages/Signin/index.jsx
@@ -1,9 +1,43 @@
+import { useState } from 'react';
+import './signin.css';
+
+import logo from '../../assets/logo.png';
+
+import { Link } from 'react-router-dom';
 
 function Signin(){
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
     return(
-        <>
-            <h1>Pagina Login</h1>
-        </>
+        <div className='container-center' >
+            <div className='login' >
+                <div className='login-area' >
+                    <img src={logo} alt="Logo do sistema" />
+                </div>
+                <form>
+
+                    <h1>Entrar</h1>
+                    <input 
+                        type="text" 
+                        placeholder='email@email.com' 
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+
+                    <input 
+                        type="text" 
+                        placeholder='******' 
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+
+                    <button>Acessar</button>
+                </form>
+
+                <Link to="/register" > Criar uma conta </Link>
+            </div>
+        </div>
     )
 }
 
