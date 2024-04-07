635ab903c7349c8a79ac8a5bc6eb09b83ceb8f52
DevMessias <messiasbento944@gmail.com>
2024-04-07 17:56:26 -0300

    Feat: Estilizando tabela

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,6 +1,12 @@
 import {useContext} from 'react';
 import { AuthContext } from '../../contexts/auth';
 import Header from '../../components/Header';
+import Title from '../../components/Title';
+import {FiPlus, FiMessageSquare, FiSearch, FiEdit2} from 'react-icons/fi';
+
+import {Link} from 'react-router-dom';
+
+import './dashboard.css'
 
 function Dashboard(){
 
@@ -14,8 +20,68 @@ function Dashboard(){
         <div>
             <Header/>
 
-            <h1>Dashboard</h1>
-            <button onClick={handleLogout} >Sair da conta</button>
+            <div className='content' >
+                <Title name="Dashboard" >
+                    <FiMessageSquare size={25} />
+                    
+                </Title>
+
+                <>
+                    <Link to="/new" className='new' >
+                        <FiPlus color='#FFF' size={25} />
+                        Novo chamado
+                    </Link>
+                    
+                    <table>
+                      <thead>
+                        <tr>
+                            <th scope='col' >Cliente</th>
+                            <th scope='col' >Assunto</th>
+                            <th scope='col' >Status</th>
+                            <th scope='col' >Cadastrado em</th>
+                            <th scope='col' >#</th>
+                        </tr>
+                      </thead> 
+                      <tbody>
+                        <tr>
+                            <td  data-label="Cliente" >Mercado Esquina</td>
+                            <td  data-label="Assunto" >Suporte</td>
+                            <td  data-label="Status"  >
+                                <span className='badge' style={{backgroundColor: '#999'}}  >Em Aberto</span>
+                            </td>
+                            <td  data-label="Cadastrado" >12/09/2020</td>
+                            <td  data-label="#" >
+                                <button className='action' style={{backgroundColor: '#3583f6'}} >
+                                    <FiSearch color='#FFF' size={17} />
+                                </button>
+                                <button className='action' style={{backgroundColor: '#f6a935'}}>
+                                    <FiEdit2 color='#FFF' size={17}  />
+                                </button>
+                            </td>
+                        </tr>
+
+
+                        <tr>
+                            <td  data-label="Cliente" >Teste TR</td>
+                            <td  data-label="Assunto" >Suporte</td>
+                            <td  data-label="Status">
+                                <span className='badge' style={{backgroundColor: '#999'}}  >Em Aberto</span>
+                            </td>
+                            <td  data-label="Cadastrado" >12/09/2020</td>
+                            <td  data-label="#" >
+                                <button className='action' style={{backgroundColor: '#3583f6'}} >
+                                    <FiSearch color='#FFF' size={17} />
+                                </button>
+                                <button className='action' style={{backgroundColor: '#f6a935'}}>
+                                    <FiEdit2 color='#FFF' size={17}  />
+                                </button>
+                            </td>
+                        </tr>
+                      </tbody> 
+                    </table>
+                </>
+            </div>
+           
         </div>
     )
 }
